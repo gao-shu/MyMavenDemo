258686cfe4e50e3867705b155e0daabfc5658a47
gao-shu <13298102001@163.com>
2022-06-22 17:57:24 +0800

    更新

---
+++ spring-boot-demo1/src/test/java/com/example/springbootdemo1/SpringBootDemo1ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.springbootdemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println(123);
+    }
+
+}
